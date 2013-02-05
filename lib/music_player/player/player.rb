7eb9f7a83016e07577612285cf8ce3c18aa11dea
alexeyfrank <alexeyfrank@gmail.com>
2013-02-05 12:20:03 +0400

    fix bug in music_player

--- lib/music_player/player/player.rb
+++ lib/music_player/player/player.rb
@@ -14,7 +14,7 @@ module AudioClient
           `cvlc #{@config[:playlist_path]}`
         end
         
-        save_pid @pid
+        # save_pid @pid
       end
       
       def stop
@@ -24,20 +24,26 @@ module AudioClient
       
       
       def stop_all_processes
-        pids_path = "#{Application.base_path}/pids/"
-        FileUtils.mkdir_p(pids_path) unless Dir.exist?(pids_path)
+        #pids_path = "#{Application.base_path}/pids/"
+        #FileUtils.mkdir_p(pids_path) unless Dir.exist?(pids_path)
         
-        pids = Dir.entries(pids_path).reject {|f| f == '.' || f == '..' }
-        puts pids.inspect
+        pids = get_vlc_pids
         pids.each do |pid|
           `kill #{pid}`
-          FileUtils.rm "#{pids_path}#{pid}"
+          #Process.kill("TERM", pid)
+          #FileUtils.rm "#{pids_path}#{pid}"
         end
       end
       
       def save_pid(pid)
-        file_path = "#{Application.base_path}/pids/#{pid}"
-        FileUtils.touch(file_path) unless File.exist?(file_path)
+        #file_path = "#{Application.base_path}/pids/#{pid}"
+        #FileUtils.touch(file_path) unless File.exist?(file_path)
+      end
+      
+      private 
+      def get_vlc_pids
+        str = `ps aux | grep vlc | grep -v grep | awk '{ print $2 }'`
+        str.split(/\n/)
       end
       
     end
